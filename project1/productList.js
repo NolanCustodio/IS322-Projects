060841a9dc58ff062c25cb4f23be4552279d50cf
NolanCustodio <nac53@njit.edu>
2021-02-18 23:30:09 -0500

    project 1 is working need to get project specifics done

--- project1/productList.js
+++ project1/productList.js
@@ -1,59 +1,70 @@
-(function () {
 
-    const mockDB = [
-        {}
-    ]
+(function (){
+    var mockDB = [
+        {_id: '1', name: 'Wooden Hanger', published: true, image: 'https://tinyurl.com/v46ysl8o'}
 
-    function renderList (results){
-        let tableBody = document.querySelector('#resultsTable tbody');
+
+        // { _id: '123', name: 'Article 1', published: true },
+		// { _id: '583', name: 'Article 2', published: true },
+		// { _id: '954', name: 'Article 3', published: false },
+		// { _id: '384', name: 'Article 4', published: false },
+		// { _id: '183', name: 'Article 5', published: true },
+		// { _id: '007', name: 'Article 6', published: false },
+		// { _id: '304', name: 'Article 7', published: true },
+		// { _id: '729', name: 'Article 8', published: false },
+		// { _id: '734', name: 'Article 9', published: true },
+    ];
+
+    function renderList(results) {
+        var tableBody = document.querySelector('#results-table tbody');
 
         tableBody.innerHTML = '';
 
-        let tableRows = results.map(function (result, index){
-            return '<tr><td>' + index + '</td><td>" + result.name' + '</td><td>' +
-                result._id + '</td><td>' + result.published + '</td></tr>';
+        var tableRows = results.map(function (result, index) {
+            return '<tr><td>' + index + '</td><td>' + result.name + '</td><td>' + 
+            result._id + '</td><td>' + result.published + '</td></tr>';
         });
 
-        tableRows.forEach(function (row) {
+        tableRows.forEach(function (row){
             tableBody.innerHTML += row;
-        })
+        });
     }
 
     renderList(mockDB);
 
-    function orderBy(sortValue){
-        let sortedResults = (sortValue === 'name') ?
+    function orderBy(sortValue) {
+        var sortedResults = (sortValue === 'name') ?
             mockDB.sort(function (a,b) {
-                let nameA = a.name.toUpperCase();
-                let nameB = b.name.toUpperCase();
+                var nameA = a.name.toUpperCase();
+                var nameB = b.name.toUpperCase();
 
-                if (nameA < nameB){
+                if (nameA < nameB) {
                     return -1;
                 }
-                if (nameA > nameB){
+                if (nameA > nameB)  {
                     return 1;
                 }
-            }) :
+            }):
             mockDB.sort(function (a,b) {
-                return a[sortValue] - b[sortValue];
+                returna[sortValue] - b[sortValue];
             });
-            renderList(sortedResults);
+        renderList(sortedResults);
     }
 
-    document.querySelector('#orderBy').addEventListener('change',function(event){
-        orderBy(event.target.value);
-    })
+    document.querySelector('#orderBy').addEventListener('change', function(event) {
+        orderBy(event,target,value);
+    });
 
     function togglePublished(showPublished) {
-        let filteredResults = mockDB.filter(function (result) {
-            return showPublsihed || result.published;
+        var filteredResults = mockDB.filter(function (result) {
+            return showPublished || result.published;
         });
         renderList(filteredResults);
     }
-    
-    document.querySelector('#published').addEventListener('change', function(event) {
-        let value= event.target.value === 'true';
-        togglePublished(value);
-    });
 
-}) ();
+    document.querySelector('#published').addEventListener('change', function(event) {
+        var value = event.target.value === 'true';
+        togglePublished(value);
+    })
+
+})();
