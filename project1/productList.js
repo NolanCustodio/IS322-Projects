102c016f3f7b7866f143cdeb7d2b80daeeb9e21a
NolanCustodio <nac53@njit.edu>
2021-02-21 18:55:10 -0500

    finished making cards responsive just need to make option bar responsive/ move from top to side

--- project1/productList.js
+++ project1/productList.js
@@ -1,7 +1,16 @@
 
 (function (){
+
+    //Image links cannot be tiny.urls 
     var mockDB = [
-        {_id: '1', name: 'Wooden Hanger', published: true, image: 'https://tinyurl.com/v46ysl8o'}
+        {_id: '1', name: 'Wooden Hanger', price: '$200', published: true, image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F-quO1DzKNljY%2FTbhzulo8g8I%2FAAAAAAAABfU%2FIOFZTBhgazA%2Fs1600%2FWooden-Hanger-for-Clothes-MC010-.jpg&f=1&nofb=1"},
+        {_id: '1', name: 'Wooden Hanger', price: '$200', published: true, image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F-quO1DzKNljY%2FTbhzulo8g8I%2FAAAAAAAABfU%2FIOFZTBhgazA%2Fs1600%2FWooden-Hanger-for-Clothes-MC010-.jpg&f=1&nofb=1"},
+        {_id: '1', name: 'Wooden Hanger', price: '$200', published: true, image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F-quO1DzKNljY%2FTbhzulo8g8I%2FAAAAAAAABfU%2FIOFZTBhgazA%2Fs1600%2FWooden-Hanger-for-Clothes-MC010-.jpg&f=1&nofb=1"},
+        {_id: '1', name: 'Wooden Hanger', price: '$200', published: true, image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F-quO1DzKNljY%2FTbhzulo8g8I%2FAAAAAAAABfU%2FIOFZTBhgazA%2Fs1600%2FWooden-Hanger-for-Clothes-MC010-.jpg&f=1&nofb=1"},
+        {_id: '1', name: 'Wooden Hanger', price: '$200', published: true, image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F-quO1DzKNljY%2FTbhzulo8g8I%2FAAAAAAAABfU%2FIOFZTBhgazA%2Fs1600%2FWooden-Hanger-for-Clothes-MC010-.jpg&f=1&nofb=1"},
+        {_id: '1', name: 'Wooden Hanger', price: '$200', published: true, image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F-quO1DzKNljY%2FTbhzulo8g8I%2FAAAAAAAABfU%2FIOFZTBhgazA%2Fs1600%2FWooden-Hanger-for-Clothes-MC010-.jpg&f=1&nofb=1"},
+        {_id: '1', name: 'Wooden Hanger', price: '$200', published: true, image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F-quO1DzKNljY%2FTbhzulo8g8I%2FAAAAAAAABfU%2FIOFZTBhgazA%2Fs1600%2FWooden-Hanger-for-Clothes-MC010-.jpg&f=1&nofb=1"},
+        {_id: '2', name: 'Nice Hanger', price: '$300', published: true, image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F-quO1DzKNljY%2FTbhzulo8g8I%2FAAAAAAAABfU%2FIOFZTBhgazA%2Fs1600%2FWooden-Hanger-for-Clothes-MC010-.jpg&f=1&nofb=1"}
 
 
         // { _id: '123', name: 'Article 1', published: true },
@@ -21,7 +30,7 @@
         tableBody.innerHTML = '';
 
         var tableRows = results.map(function (result, index) {
-            return '<tr><td>' + index + '</td><td>' + result.name + '</td><td>' + 
+            return '<tr class="wow"><td>' + index + '</td><td>' + result.name + '</td><td>' + 
             result._id + '</td><td>' + result.published + '</td></tr>';
         });
 
@@ -30,7 +39,26 @@
         });
     }
 
-    renderList(mockDB);
+    // renderList(mockDB);
+
+    function renderCards(results) {
+        var products = document.querySelector('#all-cards');
+
+        products.innerHTML = '';
+
+        //The link for image sources need to be link to direct images on the internet
+        var cards = results.map(function (result, index) {
+            return '<div class="card d-flex"> <image class="image" src=' + result.image + 
+            '<div class="card-body"><p class="name">' + result.name + 
+            '</p><p class="price">' + result.price + '</p></div></div>'
+        });
+
+        cards.forEach(function (row) {
+            products.innerHTML += row;
+        });
+    }
+
+    renderCards(mockDB);
 
     function orderBy(sortValue) {
         var sortedResults = (sortValue === 'name') ?
